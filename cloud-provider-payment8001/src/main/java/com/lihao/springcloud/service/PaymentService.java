cb11a4ef5546e6dc8837803dcfc2bc35c123691c
lihao <184505653@qq.com>
2022-07-12 21:44:27 +0800

    服务注册与发现

--- cloud-provider-payment8001/src/main/java/com/lihao/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/lihao/springcloud/service/PaymentService.java
@@ -4,7 +4,7 @@ package com.lihao.springcloud.service;
 import com.lihao.springcloud.entities.payment;
 
 public interface PaymentService {
-    public int create(payment payment);
+     int create(payment payment);
 
     public payment getPaymentById(Long id);
 }
